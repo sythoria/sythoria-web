3f4e871c5605504cc701aabaa8113d03e4c41cd8
DenysHandziichuk <fleymidesign@gmail.com>
2026-05-20 02:39:37 -0400

    feat: create index file for component exports in chat UI

---
+++ src/components/chat/ui/index.ts
@@ -0,0 +1,7 @@
+export type { Toast } from "./Toast";
+export { parseApiError, ToastContainer } from "./Toast";
+export { Spinner } from "./Spinner";
+export { Skeleton, SidebarSkeleton, MessageSkeleton, ModelCardSkeleton } from "./Skeleton";
+export { Modal, ConfirmModal, RenameChatModal } from "./Modal";
+export { Switch } from "./Switch";
+export { ErrorBoundary } from "./ErrorBoundary";
