71f88c2d70f4903c0bf13403ed0c69fe6aa44be1
DenysHandziichuk <fleymidesign@gmail.com>
2026-05-29 02:14:50 -0400

    feat: add index file to export store hooks and types

---
+++ src/store/index.ts
@@ -0,0 +1,5 @@
+export { useChatStore } from "./useChatStore";
+export { useModelStore } from "./useModelStore";
+export { useSearchStore } from "./useSearchStore";
+export { useUIStore } from "./useUIStore";
+export type { LoadingKey } from "./useChatStore";
