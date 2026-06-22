329caea9eb3beb4160bd8d5664025b273d202ed9
DenysHandziichuk <fleymidesign@gmail.com>
2026-06-22 17:12:38 -0400

    refactor: remove index.ts

--- src/store/index.ts
+++
@@ -1,5 +0,0 @@
-export { useChatStore } from "./useChatStore";
-export { useModelStore } from "./useModelStore";
-export { useSearchStore } from "./useSearchStore";
-export { useUIStore } from "./useUIStore";
-export type { LoadingKey } from "./useChatStore";
